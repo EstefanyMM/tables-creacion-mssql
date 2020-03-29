124cb35b6237ecc2f27e7efa27a3e64743d87d27
EstefanyMoncada <estefanymassiel.31@gmail.com>
2020-03-28 20:25:22 -0600

    creacion ddl tablas y csv

---
+++ TRUNCATE TABLES.sql
@@ -0,0 +1,23 @@
+
+TRUNCATE TABLE example.Customers
+
+TRUNCATE TABLE example.Categories
+
+TRUNCATE TABLE example.Employees
+
+TRUNCATE TABLE example.OrderDetails
+
+TRUNCATE TABLE example.[Order]
+
+TRUNCATE TABLE example.Products
+
+TRUNCATE TABLE example.Shippers
+
+TRUNCATE TABLE example.Suppliers
+
+ALTER TABLE example.Employees
+ALTER COLUMN Notes NVARCHAR(max)
+
+
+
+
